e7801e53902aea0c885fe3c2a832fbe1ecd47180
shrfrn <shrfrn@gmail.com>
2024-03-31 15:32:27 +0300

    Add API response caching with invalidation

--- js/services/stock.service.js
+++ js/services/stock.service.js
@@ -1,6 +1,14 @@
 'use strict'
 
+var gCache = loadFromStorage('stocksCache') || {}
+
 function getQuotes(onSuccess, symbol) {
+    
+    if(gCache[symbol] && Date.now() - gCache[symbol].ts < 10_000) {
+        console.log('From Cache')
+        onSuccess(gCache[symbol].data)
+        return
+    }
     const url = `https://api.polygon.io/v2/aggs/ticker/${symbol}/range/1/day/2023-01-09/2023-02-09?adjusted=true&sort=asc&limit=120&apiKey=30o6WIqkke5zq1mfL0S6YF05n10DJe4o`
 
     $.get(url, rawData => {
@@ -10,8 +18,10 @@ function getQuotes(onSuccess, symbol) {
                 price: res.c,
             }
         })
+        gCache[symbol] = { data, ts: Date.now() }
+
+        saveToStorage('stocksCache', gCache)
+        console.log('From Network')
         onSuccess(data)
     })
-    // const rawData = loadFromStorage('quotes')
-    // return onSuccess(data)
 }
