4af729a38427d94eb7b996509b411ccc92e00f11
shrfrn <shrfrn@gmail.com>
2024-03-31 15:03:40 +0300

    Render data to DOM

--- js/stock.controller.js
+++ js/stock.controller.js
@@ -6,5 +6,13 @@ function onGetQuotes() {
 
 function renderQuotes(data) {
     console.log(data)
+    const elList = document.querySelector('.quote-list')
+    const strHtml = data.map(quote => `
+        <li>
+            <span class="quote-date">${quote.date}</span>
+            <span class="quote-price">${quote.price}</span>
+        </li>`).join('')
+    
+    elList.innerHTML = strHtml
     // saveToStorage('quotes', data)
 }
