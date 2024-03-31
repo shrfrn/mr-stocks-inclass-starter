9bc41cfde312b57a6f67635e51b3de06a6814203
shrfrn <shrfrn@gmail.com>
2024-03-31 15:42:29 +0300

    Add chart from chart JS

--- js/stock.controller.js
+++ js/stock.controller.js
@@ -1,19 +1,49 @@
 'use strict'
 
 function onGetQuotes(ev) {
-    const symbol = ev.target.value
-    getQuotes(renderQuotes, symbol)
+	const symbol = ev.target.value
+	getQuotes(renderQuotes, symbol)
 }
 
 function renderQuotes(data) {
-    console.log(data)
-    const elList = document.querySelector('.quote-list')
-    const strHtml = data.map(quote => `
+	console.log(data)
+	const elList = document.querySelector('.quote-list')
+	const strHtml = data
+		.map(
+			quote => `
         <li>
             <span class="quote-date">${quote.date}</span>
             <span class="quote-price">${quote.price}</span>
-        </li>`).join('')
-    
-    elList.innerHTML = strHtml
-    // saveToStorage('quotes', data)
+        </li>`
+		)
+		.join('')
+
+	elList.innerHTML = strHtml
+	rederChart(data)
+}
+
+function rederChart(data) {
+	const ctx = document.querySelector('canvas')
+
+	new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: data.map(quote => quote.date),
+			datasets: [
+				{
+					label: '# of Votes',
+					data: data.map(quote => quote.price),
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {
+            maintainAspectRatio: false,
+			scales: {
+				y: {
+					beginAtZero: false,
+				},
+			},
+		},
+	})
 }
