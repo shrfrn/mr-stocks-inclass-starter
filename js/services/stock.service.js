f02e618784a99474f52ee555f8fc8ec125cbd035
shrfrn <shrfrn@gmail.com>
2024-03-31 14:46:22 +0300

    Adjust data format

--- js/services/stock.service.js
+++ js/services/stock.service.js
@@ -2,7 +2,13 @@
 
 function getQuotes(onSuccess) {
     const rawData = loadFromStorage('quotes')
-    return onSuccess(rawData)
+    const data = rawData.results.map(res => {
+        return {
+            date: new Date(res.t).toISOString().slice(0, 10),
+            price: res.c,
+        }
+    })
+    return onSuccess(data)
     
     const url = `https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-02-09?adjusted=true&sort=asc&limit=120&apiKey=30o6WIqkke5zq1mfL0S6YF05n10DJe4o`
     $.get(url, onSuccess)
